592edcf33a20d7cbac4b30bd48ab760a74019174
Eldar Akavov <akavoveldar3005@gmail.com>
2020-10-12 20:56:20 +0300

    fix(script.js): new fix

--- script/script.js
+++ script/script.js
@@ -1,39 +1,37 @@
 window.addEventListener('DOMContentLoaded', () => {
-    'use strict';
-    // Timer
-    function countTimer(deadline) {
-        const timerHours = document.querySelector('#timer-hours'),
-            timerMinutes = document.querySelector('#timer-minutes'),
-            timerSeconds = document.querySelector('#timer-seconds');
-        const zero = n => (n < 10 ? `0${n}` : n);
-        function getTimeRemaining() {
-            const dateStop = new Date(deadline).getTime(),
-                dateNow = new Date().getTime(),
-                timeRemaining = (dateStop - dateNow) / 1000,
-                seconds = Math.floor(timeRemaining % 60),
-                minutes = Math.floor((timeRemaining / 60) % 60),
-                hours = Math.floor(timeRemaining / 60 / 60);
-            return { timeRemaining, hours, minutes, seconds };
-        }
-
-        function updateClock() {
-            const timer = getTimeRemaining();
-
-            const interval = setInterval(updateClock, 1000);
-
-            if (timer.timeRemaining > 0) {
-              timerHours.textContent = zero(timer.hours);
-              timerMinutes.textContent = zero(timer.minutes);
-              timerSeconds.textContent = zero(timer.seconds);
-            } else {
-              timerHours.textContent = '00';
-              timerMinutes.textContent = '00';
-              timerSeconds.textContent = '00';
-              clearInterval(interval);
-            }
-        }
-        updateClock();
+'use strict';
+// Timer
+function countTimer(deadline) {
+    const timerHours = document.querySelector('#timer-hours'),
+        timerMinutes = document.querySelector('#timer-minutes'),
+        timerSeconds = document.querySelector('#timer-seconds');
+    const zero = n => (n < 10 ? `0${n}` : n);
+    function getTimeRemaining() {
+      const dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000,
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor((timeRemaining / 60) % 60),
+        hours = Math.floor(timeRemaining / 60 / 60);
+        return { timeRemaining, hours, minutes, seconds };
     }
 
-    countTimer('19 october 2020');
+    function updateClock() {
+      const timer = getTimeRemaining();
+
+      if (timer.timeRemaining > 0) {
+          timerHours.textContent = zero(timer.hours);
+          timerMinutes.textContent = zero(timer.minutes);
+          timerSeconds.textContent = zero(timer.seconds);
+      } else {
+          timerHours.textContent = '00';
+          timerMinutes.textContent = '00';
+          timerSeconds.textContent = '00';
+          clearInterval(interval);
+      }
+    }
+    const interval = setInterval(updateClock, 1000);
+}
+
+    countTimer('11 october 2020');
 });
--- secondPart/script.js
+++ secondPart/script.js
@@ -5,33 +5,15 @@ const date = new Date(),
   timeRemaining = (nextDate - date) / 1000,
   hours = date.getHours(),
   time = date.toLocaleTimeString('en'),
+  today = date.toLocaleString('ru', { weekday: 'long' }),
   timer = Math.floor(timeRemaining / 60 / 60 / 24);
-let greeting;
-if (hours >= 5 && hours < 12) {
-  greeting = 'Доброе утро';
-} else if (hours >= 12 && hours < 18) {
-  greeting = 'Добрый день';
-} else if (hours >= 18 && hours < 24) {
-  greeting = 'Добрый вечер';
-} else {
-  greeting = 'Доброй ночи';
-}
-let day = date.getDay();
-switch (day)
-  {
-    case 0: day = "Воскресенье"; break;
-    case 1: day = "Понедельник"; break;
-    case 2: day = "Вторник"; break;
-    case 3: day = "Среда"; break;
-    case 4: day = "Четверг"; break;
-    case 5: day = "Пятница"; break;
-    case 6: day = "Суббота"; break;
-  }
-
+let greeting = (hours >= 5 && hours < 12) ? 'Доброе утро' :
+  (hours >= 12 && hours < 18) ? 'Добрый день' :
+  (hours >= 18 && hours < 24) ? 'Добрый вечер' : 'Доброй ночи';
 const marryXmas = document.querySelector('.marryXmas');
 marryXmas.innerHTML = `${greeting}
 <br>
-Сегодня: ${day}
+Сегодня: ${today}
 <br>
 Текущее время: ${time}
 <br>

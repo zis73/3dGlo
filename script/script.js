38fdfe635d27c4feda4d8e0ca794c399a3977d77
Eldar Akavov <akavoveldar3005@gmail.com>
2020-10-14 15:24:24 +0300

    feat(script.js): new feat

--- script/script.js
+++ script/script.js
@@ -1,37 +1,93 @@
 window.addEventListener('DOMContentLoaded', () => {
-'use strict';
-// Timer
-function countTimer(deadline) {
-    const timerHours = document.querySelector('#timer-hours'),
-        timerMinutes = document.querySelector('#timer-minutes'),
-        timerSeconds = document.querySelector('#timer-seconds');
-    const zero = n => (n < 10 ? `0${n}` : n);
-    function getTimeRemaining() {
-      const dateStop = new Date(deadline).getTime(),
-        dateNow = new Date().getTime(),
-        timeRemaining = (dateStop - dateNow) / 1000,
-        seconds = Math.floor(timeRemaining % 60),
-        minutes = Math.floor((timeRemaining / 60) % 60),
-        hours = Math.floor(timeRemaining / 60 / 60);
-        return { timeRemaining, hours, minutes, seconds };
+  'use strict';
+  // Timer
+  const countTimer = (deadline) => {
+        const timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
+
+        const zero = n => (n < 10 ? `0${n}` : n);
+
+        const getTimeRemaining = () => {
+        const dateStop = new Date(deadline).getTime(),
+            dateNow = new Date().getTime(),
+            timeRemaining = (dateStop - dateNow) / 1000,
+            seconds = Math.floor(timeRemaining % 60),
+            minutes = Math.floor((timeRemaining / 60) % 60),
+            hours = Math.floor(timeRemaining / 60 / 60);
+            return { timeRemaining, hours, minutes, seconds };
+        }
+
+        const updateClock = () => {
+        const timer = getTimeRemaining();
+
+        if (timer.timeRemaining > 0) {
+            timerHours.textContent = zero(timer.hours);
+            timerMinutes.textContent = zero(timer.minutes);
+            timerSeconds.textContent = zero(timer.seconds);
+        } else {
+            timerHours.textContent = '00';
+            timerMinutes.textContent = '00';
+            timerSeconds.textContent = '00';
+            clearInterval(interval);
+        }
+        }
+        const interval = setInterval(updateClock, 1000);
+  }
+
+  countTimer('15 october 2020');
+
+  //Menu
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+      menu = document.querySelector('menu'),
+      menuItems = menu.querySelectorAll('ul>li'),
+      closeBtn = document.querySelector('.close-btn');
+
+    const handlerMenu = () => {
+      menu.classList.toggle('active-menu');
     }
 
-    function updateClock() {
-      const timer = getTimeRemaining();
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
 
-      if (timer.timeRemaining > 0) {
-          timerHours.textContent = zero(timer.hours);
-          timerMinutes.textContent = zero(timer.minutes);
-          timerSeconds.textContent = zero(timer.seconds);
+    menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+  };
+  toggleMenu();
+
+  //popup
+
+  const togglePopUp = () => {
+    const popup = document.querySelector('.popup'),
+      popupClose = document.querySelector('.popup-close'),
+      popupBtn = document.querySelectorAll('.popup-btn');
+    
+    let count = 0;
+    let popupInterval;
+    const popupAnimation = () => {
+      count++;
+      popup.style.cssText = `position: relative`;
+      popup.style.top = count + 'px';
+      if (count < 300) {
+        popupInterval = requestAnimationFrame(popupAnimation);
       } else {
-          timerHours.textContent = '00';
-          timerMinutes.textContent = '00';
-          timerSeconds.textContent = '00';
-          clearInterval(interval);
-      }
-    }
-    const interval = setInterval(updateClock, 1000);
-}
+        cancelAnimationFrame(popupInterval);
+            }
+          };
 
-    countTimer('11 october 2020');
+    popupInterval = requestAnimationFrame(popupAnimation);
+    
+    popupBtn.forEach((elem) => {
+      elem.addEventListener('click', () => {
+          popup.style.display = 'block';
+          popupAnimation();
+      });
+    });
+    popupClose.addEventListener('click', () => {
+      popup.style.display = 'none';
+    })
+  }
+    
+
+  togglePopUp();
 });
